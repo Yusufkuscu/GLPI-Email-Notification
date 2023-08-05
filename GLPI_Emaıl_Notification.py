9c3dca89bf7ac60bbd7c087de9d694ce2416bd48
YUSUF KUSCU <99915079+Yusufkuscu@users.noreply.github.com>
2023-08-05 12:11:11 +0300

    Update GLPI_Emaıl_Notification.py

--- GLPI_Emaıl_Notification.py
+++ GLPI_Emaıl_Notification.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from tkinter import messagebox
 import threading
 
-# GPIO modunu ve pin numaras�n� ayarlay�n
+# GPIO modunu ve pin numarasını ayarlayın
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 YESIL_LED = 22
@@ -13,41 +13,41 @@ KIRMIZI_LED = 24
 GPIO.setup(YESIL_LED, GPIO.OUT)
 GPIO.setup(KIRMIZI_LED, GPIO.OUT)
 
-# LED'leri varsay�lan olarak kapat
+# LED'leri varsayılan olarak kapat
 GPIO.output(YESIL_LED, False)
 GPIO.output(KIRMIZI_LED, False)
 
-# Outlook IMAP hesap bilgilerini varsay�lan de�erlerle ayarlay�n
+# Outlook IMAP hesap bilgilerini varsayılan değerlerle ayarlayın
 email_address = ""  # e-posta adresi
-password = ""  # Outlook hesab�n�z�n �ifresi
+password = ""  # Outlook hesabınızın şifresi
 
-# Varsay�lan LED s�relerini ve e-posta kontrol s�resini ayarlay�n (saniye cinsinden)
+# Varsayılan LED sürelerini ve e-posta kontrol süresini ayarlayın (saniye cinsinden)
 default_green_led_time = 5
 default_red_led_time = 2
 default_email_check_interval = 15
 
 def check_new_emails():
     try:
-        # Outlook IMAP sunucusuna ba�lanma
+        # Outlook IMAP sunucusuna bağlanma
         mail = imaplib.IMAP4_SSL("imap-mail.outlook.com")
 
-        # Giri� yapma
+        # Giriş yapma
         mail.login(email_address, password)
 
-        # Gelen kutusuna eri�im
+        # Gelen kutusuna erişim
         mail.select("inbox")
 
-        # Yeni e-posta kontrol� yapma
+        # Yeni e-posta kontrolü yapma
         status, messages = mail.search(None, "UNSEEN")
 
-        # Yeni e-posta geldiyse ye�il LED'i a�
+        # Yeni e-posta geldiyse yeşil LED'i aç
         if status == "OK" and messages[0]:
             return True
         else:
             return False
 
     except Exception as e:
-        print("E-posta kontrol� yap�l�rken bir hata olu�tu:", e)
+        print("E-posta kontrolü yapılırken bir hata oluştu:", e)
         return False
 
 def save_settings_to_file(green_led_time, red_led_time, email_check_interval):
@@ -66,30 +66,30 @@ def load_settings_from_file():
                 default_red_led_time = int(lines[3].strip())
                 default_email_check_interval = int(lines[4].strip())
             else:
-                print("Dosya i�eri�i eksik veya hatal�.")
+                print("Dosya içeriği eksik veya hatalı.")
     except FileNotFoundError:
-        # E�er dosya yoksa veya hatal�ysa varsay�lan de�erleri kullanmaya devam edelim
+        # Eğer dosya yoksa veya hatalıysa varsayılan değerleri kullanmaya devam edelim
         pass
 
 def main_loop():
     try:
         while True:
-            # Yeni e-posta kontrol� yap
+            # Yeni e-posta kontrolü yap
             new_email = check_new_emails()
 
             if new_email:
-                print("�A�RI VAR!")
-                GPIO.output(KIRMIZI_LED, False)  # K�rm�z� LED'i s�nd�r
-                GPIO.output(YESIL_LED, True)  # Ye�il LED'i a�
+                print("ÇAĞRI VAR!")
+                GPIO.output(KIRMIZI_LED, False)  # Kırmızı LED'i söndür
+                GPIO.output(YESIL_LED, True)  # Yeşil LED'i aç
                 time.sleep(default_green_led_time)
-                GPIO.output(YESIL_LED, False)  # Ye�il LED'i s�nd�r
+                GPIO.output(YESIL_LED, False)  # Yeşil LED'i söndür
             else:
-                print("�A�RI YOK!")
-                GPIO.output(YESIL_LED, False)  # Ye�il LED'i s�nd�r
-                GPIO.output(KIRMIZI_LED, True)  # K�rm�z� LED'i a�
+                print("ÇAĞRI YOK!")
+                GPIO.output(YESIL_LED, False)  # Yeşil LED'i söndür
+                GPIO.output(KIRMIZI_LED, True)  # Kırmızı LED'i aç
                 time.sleep(default_red_led_time)
 
-            # Kontrol aral���n� ayarlay�n
+            # Kontrol aralığını ayarlayın
             time.sleep(default_email_check_interval)
 
     except KeyboardInterrupt:
@@ -109,52 +109,52 @@ def run_gui():
     load_settings_from_file()
 
     root = tk.Tk()
-    root.title("E-posta Bildirim ve Kontrol Uygulamas�")
+    root.title("E-posta Bildirim ve Kontrol Uygulaması")
 
-    # E-posta ayarlar� ba�l�k
-    email_settings_label = tk.Label(root, text="E-posta Ayarlar�", font=("Arial", 16))
+    # E-posta ayarları başlık
+    email_settings_label = tk.Label(root, text="E-posta Ayarları", font=("Arial", 16))
     email_settings_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-    # E-posta adresi ve �ifre giri�leri
+    # E-posta adresi ve şifre girişleri
     email_label = tk.Label(root, text="E-posta Adresi:")
     email_label.grid(row=1, column=0)
     email_entry = tk.Entry(root)
     email_entry.grid(row=1, column=1)
     email_entry.insert(0, email_address)
 
-    password_label = tk.Label(root, text="�ifre:")
+    password_label = tk.Label(root, text="Şifre:")
     password_label.grid(row=2, column=0)
     password_entry = tk.Entry(root, show="*")
     password_entry.grid(row=2, column=1)
     password_entry.insert(0, password)
 
-    # LED ayarlar� ba�l�k
-    led_settings_label = tk.Label(root, text="LED Ayarlar�", font=("Arial", 16))
+    # LED ayarları başlık
+    led_settings_label = tk.Label(root, text="LED Ayarları", font=("Arial", 16))
     led_settings_label.grid(row=3, column=0, columnspan=2, pady=10)
 
-    # Ye�il LED s�resi giri�i
-    green_led_label = tk.Label(root, text="Ye�il LED S�resi (saniye):")
+    # Yeşil LED süresi girişi
+    green_led_label = tk.Label(root, text="Yeşil LED Süresi (saniye):")
     green_led_label.grid(row=4, column=0)
     green_led_entry = tk.Entry(root)
     green_led_entry.grid(row=4, column=1)
     green_led_entry.insert(0, default_green_led_time)
 
-    # K�rm�z� LED s�resi giri�i
-    red_led_label = tk.Label(root, text="K�rm�z� LED S�resi (saniye):")
+    # Kırmızı LED süresi girişi
+    red_led_label = tk.Label(root, text="Kırmızı LED Süresi (saniye):")
     red_led_label.grid(row=5, column=0)
     red_led_entry = tk.Entry(root)
     red_led_entry.grid(row=5, column=1)
     red_led_entry.insert(0, default_red_led_time)
 
-    # E-posta kontrol s�resi giri�i
-    email_check_interval_label = tk.Label(root, text="E-posta Kontrol S�resi (saniye):")
+    # E-posta kontrol süresi girişi
+    email_check_interval_label = tk.Label(root, text="E-posta Kontrol Süresi (saniye):")
     email_check_interval_label.grid(row=6, column=0)
     email_check_interval_entry = tk.Entry(root)
     email_check_interval_entry.grid(row=6, column=1)
     email_check_interval_entry.insert(0, default_email_check_interval)
 
-    # Ayarlar� kaydet butonu
-    save_button = tk.Button(root, text="Ayarlar� Kaydet", command=update_settings)
+    # Ayarları kaydet butonu
+    save_button = tk.Button(root, text="Ayarları Kaydet", command=update_settings)
     save_button.grid(row=7, column=0, columnspan=2, pady=10)
 
     root.mainloop()
@@ -166,4 +166,3 @@ if __name__ == "__main__":
 
     run_gui()
 
-    fsafs
